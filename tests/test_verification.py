2517655b89cc18effa8a4f7b3685975f8ca72d0c
Ryan Shaffer <3620100+rmshaffer@users.noreply.github.com>
2022-08-17 10:50:56 -0400

    Implement XEB sequence generation

--- tests/test_verification.py
+++ tests/test_verification.py
@@ -33,6 +33,23 @@ class TestVerification:
         assert rav_result.compiled_sequence.get_qasm()
         assert rav_result.compiled_sequence.get_jaqal()
 
+    def test_rav_no_inverse(self) -> None:
+        num_system_qubits = 2
+        system_dimension = qubit_dimension ** num_system_qubits
+        unitary_primitives = [
+            UnitaryPrimitive(ParameterizedUnitaryDefinitions.rotation_xy()),
+            UnitaryPrimitive(ParameterizedUnitaryDefinitions.xx())]
+
+        sequence_length = 10
+        threshold = 0.0
+        rav_result = Verification.generate_rav_sequence(
+            system_dimension, unitary_primitives, sequence_length, threshold)
+        assert isinstance(rav_result, CompilerResult)
+        
+        assert rav_result.compiled_sequence.get_length() == sequence_length
+        assert rav_result.compiled_sequence.get_qasm()
+        assert rav_result.compiled_sequence.get_jaqal()
+
     def test_layered_rav(self) -> None:
         num_system_qubits = 2
         system_dimension = qubit_dimension ** num_system_qubits
@@ -58,3 +75,22 @@ class TestVerification:
             product.distance_from(Unitary.identity(system_dimension))
         assert layered_rav_result.compiled_sequence.get_qasm()
         assert layered_rav_result.compiled_sequence.get_jaqal()
+
+    def test_xeb(self) -> None:
+        num_system_qubits = 2
+        system_dimension = qubit_dimension ** num_system_qubits
+        unitary_primitive_counts = {
+            UnitaryPrimitive(ParameterizedUnitaryDefinitions.rotation_xy()): 3,
+            UnitaryPrimitive(ParameterizedUnitaryDefinitions.xx()): 1
+        }
+
+        layer_count = 10
+        xeb_result = Verification.generate_xeb_sequence(
+            system_dimension, unitary_primitive_counts, layer_count)
+        assert isinstance(xeb_result, CompilerResult)
+
+        layer_length = sum(unitary_primitive_counts.values())
+        sequence_length = xeb_result.compiled_sequence.get_length()
+        assert sequence_length == layer_length * layer_count
+        assert xeb_result.compiled_sequence.get_qasm()
+        assert xeb_result.compiled_sequence.get_jaqal()
