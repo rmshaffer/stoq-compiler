93c3d2086296f79cfd4e762219561a1219e448b6
Ryan Shaffer <3620100+rmshaffer@users.noreply.github.com>
2022-08-18 09:18:23 -0400

    Update minor version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="stoqcompiler",
-    version="0.1.0",
+    version="0.2.0",
     author="Ryan Shaffer",
     author_email="ryan@ryanshaffer.net",
     description="Toolset for stochastic approximate quantum compilation.",
